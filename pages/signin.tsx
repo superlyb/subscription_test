6f100efb5464f655b3a07b22757c3fc5e65a8395
superlyb <tudouxiong@hotmail.com>
2023-05-01 21:58:49 +0200

    Update signin.tsx

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -29,7 +29,7 @@ const SignIn = () => {
           <div className="flex flex-col space-y-4">
             <Auth
               supabaseClient={supabaseClient}
-              providers={['Apple']}
+              providers={['apple']}
               redirectTo={getURL()}
               magicLink={true}
               appearance={{
