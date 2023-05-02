1ce39f10079a866010528aba2a59e4ddd375e529
superlyb <tudouxiong@hotmail.com>
2023-05-02 09:17:38 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,28 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+const headersConfig = {
+  async headers() {
+    return [
+      {
+        // Set the Access-Control-Allow-Origin header to allow requests from site A
+        source: "https://www.bus-ai.com",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = {
+  ...nextConfig,
+  ...headersConfig,
+};
