92fbc0121aa8e8af1a76eb3bc577586226af26d4
superlyb <tudouxiong@hotmail.com>
2023-05-02 10:15:08 +0200

    Revert "Update next.config.js"
    This reverts commit 1ce39f10079a866010528aba2a59e4ddd375e529.

--- next.config.js
+++ next.config.js
@@ -3,28 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-const headersConfig = {
-  async headers() {
-    return [
-      {
-        // Set the Access-Control-Allow-Origin header to allow requests from site A
-        source: "https://www.bus-ai.com",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-        ],
-      },
-    ];
-  },
-};
-
-module.exports = {
-  ...nextConfig,
-  ...headersConfig,
-};
+module.exports = nextConfig;
